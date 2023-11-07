394ed5d474d5ac8f6e0df7a0b6241b8e554e6393
git-leon <xleonhunter@gmail.com>
2023-11-07 18:46:01 -0500

    solution

--- maven.catdogyears/README.md
+++ maven.catdogyears/README.md
@@ -1,8 +1,8 @@
 # Kata Task
 
 * I have a cat and a dog.
-* I got them at the same time as kitten/puppy. That was humanYears years ago.
-* Return their respective ages now as [humanYears,catYears,dogYears]
+* I got them at the same time as kitten/puppy. That was `humanYears` years ago.
+* Return their respective ages now as `[humanYears,catYears,dogYears]`
 
 ## NOTES:
 ```
--- maven.catdogyears/src/main/java/com/github/curriculeon/YearCalculator.java
+++ maven.catdogyears/src/main/java/com/github/curriculeon/YearCalculator.java
@@ -2,6 +2,18 @@ package com.github.curriculeon;
 
 public class YearCalculator {
     public int[] calculate(int input) {
-        return new int[0];
+        int catYears = 0;
+        int dogYears = 0;
+        if (input > 2) {
+            dogYears = 15 + 9 + (5 * (input-2));
+            catYears = 15 + 9 + (4 * (input-2));
+        } else if(input == 1){
+            dogYears = 15;
+            catYears = 15;
+        } else {
+            dogYears = 15 + 9;
+            catYears = 15 + 9;
+        }
+        return new int[]{input, catYears, dogYears};
     }
 }
