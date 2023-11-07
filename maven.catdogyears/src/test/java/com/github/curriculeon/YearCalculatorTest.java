0c455bbcf78257e7784670e7e7123e6768caf00a
git-leon <xleonhunter@gmail.com>
2023-11-07 18:47:19 -0500

    solution

--- maven.catdogyears/src/test/java/com/github/curriculeon/YearCalculatorTest.java
+++ maven.catdogyears/src/test/java/com/github/curriculeon/YearCalculatorTest.java
@@ -1,21 +1,5 @@
 package com.github.curriculeon;
 
-
-//  it("one", function() {
-//          Test.assertSimilar(humanYearsCatYearsDogYears(1), [1,15,15]);
-//          });
-//
-//          it("two", function() {
-//          Test.assertSimilar(humanYearsCatYearsDogYears(2), [2,24,24]);
-//          });
-//
-//          it("ten", function() {
-//          Test.assertSimilar(humanYearsCatYearsDogYears(10), [10,56,64]);
-//          });
-//
-//          });
-//
-
 import org.junit.Assert;
 import org.junit.Test;
 
